d7759973ad554a54f65335c2874166d164c65023
Jerry Sitati <jerryssitati@gmail.com>
2025-07-31 08:19:37 +0300

    Deploy frontend to vercel with correct API base url

---
+++ src/api.js
@@ -0,0 +1,17 @@
+// src/api.js
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+// Optional: Automatically add token from localStorage
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
+export default API;
